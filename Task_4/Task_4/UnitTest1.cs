53cf437e697e5c5438540e8701cbf9cdfae1d698
Pavlov <36180633+PavlovEAE@users.noreply.github.com>
2018-02-13 18:07:10 +0300

    добавлен шаблон для задания 4

---
+++ Task_4/Task_4/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+
+namespace Task_4
+{
+    [TestFixture]
+    public class MyFistTest
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new InternetExplorerDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+     
+
+        [Test]
+        public void TestIE()
+        {
+            driver.Url = "http://localhost/litecart/admin";
+            driver.FindElement(By.Name("username")).SendKeys("admins");
+            driver.FindElement(By.Name("password")).SendKeys("admins");
+            driver.FindElement(By.Name("login")).Click();
+            wait.Until(ExpectedConditions.TitleIs("My Store"));
+        }
+
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
