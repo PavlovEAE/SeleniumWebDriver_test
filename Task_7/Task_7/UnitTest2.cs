220ae42e7e6ee9b08821bc92f2764f1cc80d4a08
Pavlov <36180633+PavlovEAE@users.noreply.github.com>
2018-02-22 20:41:16 +0300

    начало 8го задания

---
+++ Task_7/Task_7/UnitTest2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+
+namespace csharp_example_task_8
+{
+    [TestFixture]
+    public class MyFirstTest
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new FirefoxDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+            driver.Url = "http://localhost/litecart/";
+            //эти строкина надо переделать для проверки
+            IWebElement element = driver.FindElement(By.CssSelector("div.box#box-most-popular a.link[title*='Red Duck'] div[class*=sticker]"));
+            IWebElement element2 = driver.FindElement(By.CssSelector("div.box#box-most-popular a.link[title*='Green Duck'] div[class*=sticker]"));
+            IWebElement element3 = driver.FindElement(By.CssSelector("div.box#box-most-popular a.link[title*='Yellow Duck'] div[class*=sticker]"));
+            IWebElement element4 = driver.FindElement(By.CssSelector("div.box#box-most-popular a.link[title*='Purple Duck'] div[class*=sticker]"));
+            IWebElement element5 = driver.FindElement(By.CssSelector("div.box#box-most-popular a.link[title*='Blue Duck'] div[class*=sticker]"));
+
+              // заготовка       
+             //driver.FindElements(By.CssSelector("div.box#box-most-popular a.link[title*='Blue Duck'] div[class*=sticker]"))
+            
+   
+
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
