4a6f8fc11614945e0632bb3227a4016d7e11bd7f
Pavlov <36180633+PavlovEAE@users.noreply.github.com>
2018-02-23 18:30:12 +0300

    выполнено 8 задание

--- Task_7/Task_7/UnitTest2.cs
+++ Task_7/Task_7/UnitTest2.cs
@@ -17,28 +17,41 @@ namespace csharp_example_task_8
         [SetUp]
         public void start()
         {
-            driver = new FirefoxDriver();
+            driver = new ChromeDriver();
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
             wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
 
         }
 
         [Test]
-        public void FirstTest()
+        public void FirstTest_Task_8()
         {
             driver.Url = "http://localhost/litecart/";
-            //эти строкина надо переделать для проверки
-            IWebElement element = driver.FindElement(By.CssSelector("div.box#box-most-popular a.link[title*='Red Duck'] div[class*=sticker]"));
-            IWebElement element2 = driver.FindElement(By.CssSelector("div.box#box-most-popular a.link[title*='Green Duck'] div[class*=sticker]"));
-            IWebElement element3 = driver.FindElement(By.CssSelector("div.box#box-most-popular a.link[title*='Yellow Duck'] div[class*=sticker]"));
-            IWebElement element4 = driver.FindElement(By.CssSelector("div.box#box-most-popular a.link[title*='Purple Duck'] div[class*=sticker]"));
-            IWebElement element5 = driver.FindElement(By.CssSelector("div.box#box-most-popular a.link[title*='Blue Duck'] div[class*=sticker]"));
+            var elements = driver.FindElements(By.CssSelector("div.box#box-most-popular a.link[title*='Blue Duck'] div[class*=sticker]"));
+                Assert.AreEqual(1, elements.Count);
+            var elements2 = driver.FindElements(By.CssSelector("div.box#box-most-popular a.link[title*='Purple Duck'] div[class*=sticker]"));
+                Assert.AreEqual(1, elements2.Count);
+            var elements3 = driver.FindElements(By.CssSelector("div.box#box-most-popular a.link[title*='Yellow Duck'] div[class*=sticker]"));
+                Assert.AreEqual(1, elements3.Count);
+            var elements4 = driver.FindElements(By.CssSelector("div.box#box-most-popular a.link[title*='Green Duck'] div[class*=sticker]"));
+                Assert.AreEqual(1, elements4.Count);
+            var elements5 = driver.FindElements(By.CssSelector("div.box#box-most-popular a.link[title*='Red Duck'] div[class*=sticker]"));
+                Assert.AreEqual(1, elements5.Count);
 
-              // заготовка       
-             //driver.FindElements(By.CssSelector("div.box#box-most-popular a.link[title*='Blue Duck'] div[class*=sticker]"))
-            
-   
+            var elements6 = driver.FindElements(By.CssSelector("div.box#box-campaigns a.link[title*='Yellow Duck'] div[class*=sticker]"));
+            Assert.AreEqual(1, elements3.Count);
 
+            var elements7 = driver.FindElements(By.CssSelector("div.box#box-latest-products a.link[title*='Blue Duck'] div[class*=sticker]"));
+            Assert.AreEqual(1, elements.Count);
+            var elements8 = driver.FindElements(By.CssSelector("div.box#box-latest-products a.link[title*='Purple Duck'] div[class*=sticker]"));
+            Assert.AreEqual(1, elements2.Count);
+            var elements9 = driver.FindElements(By.CssSelector("div.box#box-latest-products a.link[title*='Yellow Duck'] div[class*=sticker]"));
+            Assert.AreEqual(1, elements3.Count);
+            var elements10 = driver.FindElements(By.CssSelector("div.box#box-latest-products a.link[title*='Green Duck'] div[class*=sticker]"));
+            Assert.AreEqual(1, elements4.Count);
+            var elements11 = driver.FindElements(By.CssSelector("div.box#box-latest-products a.link[title*='Red Duck'] div[class*=sticker]"));
+            Assert.AreEqual(1, elements5.Count);
+           
         }
 
         [TearDown]
