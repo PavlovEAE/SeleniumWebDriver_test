a11de2d890c8d3e84e432af470ce3b2b923825ce
Pavlov <36180633+PavlovEAE@users.noreply.github.com>
2018-03-05 21:26:33 +0300

    выполнено 13 задание

--- Lesson_6/Task_13.cs
+++ Lesson_6/Task_13.cs
@@ -9,7 +9,7 @@ using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.IE;
 using OpenQA.Selenium.Support.UI;
 
-namespace csharp_example_task_12
+namespace csharp_example_task_13
 {
     [TestFixture]
     public class Lesson_7
@@ -20,9 +20,9 @@ namespace csharp_example_task_12
         [SetUp]
         public void start()
         {
-            driver = new FirefoxDriver();
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(7);
-            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(7));
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2);
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(2));
 
         }
 
@@ -35,15 +35,43 @@ namespace csharp_example_task_12
             //driver.FindElement(By.Name("password")).SendKeys("admin");
             //driver.FindElement(By.Name("login")).Click();
 
-            Assert.DoesNotThrow(() => driver.FindElement(By.XPath("//h3[contains(.,'Categories')]")));
-            driver.FindElement(By.CssSelector("ul.listing-wrapper li.product")).Click();
+            for (int i = 1; i <= 3; i++)
+            {
+                Assert.DoesNotThrow(() => driver.FindElement(By.XPath("//div[@id='box-most-popular']/h3[contains(.,'Most Popular')]")));
+                driver.FindElement(By.CssSelector("ul.listing-wrapper li.product")).Click();
 
-            Assert.DoesNotThrow(() => driver.FindElement(By.XPath("//h1[contains(.,'Duck')]")));
-            var quantityInCart = driver.FindElement(By.CssSelector("div#cart span.quantity"));
-            driver.FindElement(By.Name("add_cart_product")).Click();
-            wait.Until(ExpectedConditions.StalenessOf(quantityInCart));
+                Assert.DoesNotThrow(() => driver.FindElement(By.XPath("//h1[contains(.,'Duck')]")));
 
-            driver.FindElement(By.Id("logotype-wrapper")).Click();
+                //проверяем желтую утку
+                if (driver.FindElements(By.Name("options[Size]")).Count != 0)
+                {
+                    var yellowDuckSizes = driver.FindElement(By.Name("options[Size]"));
+                    var yellowDuckSize = new SelectElement(yellowDuckSizes);
+                    yellowDuckSize.SelectByValue("Small");
+                }
+                //получаем количество в товара корзине
+                var quantityInCart = driver.FindElement(By.CssSelector("div#cart span.quantity"));
+                driver.FindElement(By.Name("add_cart_product")).Click();
+                string iString = i.ToString();
+                wait.Until(ExpectedConditions.TextToBePresentInElement(quantityInCart, iString));
+                var quantityInCartNew = driver.FindElement(By.CssSelector("div#cart span.quantity")).Text;
+                driver.FindElement(By.Id("logotype-wrapper")).Click();
+
+            }
+            Assert.DoesNotThrow(() => driver.FindElement(By.XPath("//div[@id='box-most-popular']/h3[contains(.,'Most Popular')]")));
+            driver.FindElement(By.XPath("//div[@id='cart']/a[contains(.,'Checkout »')]")).Click();
+
+            Assert.DoesNotThrow(() => driver.FindElement(By.XPath("//div[@id='box-checkout-customer']/h2[contains(.,'Customer Details')]")));
+
+            while (driver.FindElements(By.Name("remove_cart_item")).Count >0)
+            {
+                var duck = driver.FindElement(By.Name("remove_cart_item"));
+                duck.Click();
+                wait.Until(ExpectedConditions.StalenessOf(duck));
+
+            }
+            Assert.DoesNotThrow(() => driver.FindElement(By.XPath("//p[contains(.,'There are no items in your cart.')]")));
+
 
         }
         [TearDown]
