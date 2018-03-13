5e43380c65924fe8ddad52060b22bbb2fae156ba
Pavlov <36180633+PavlovEAE@users.noreply.github.com>
2018-03-13 14:17:53 +0300

    выполнено 17 задание

--- Lesson_6/Task_17.cs
+++ Lesson_6/Task_17.cs
@@ -12,57 +12,61 @@ namespace csharp_example_Task_17
     [TestFixture]
     public class MyFirstTest
     {
-        private EventFiringWebDriver driver;
+        private IWebDriver driver;
         private WebDriverWait wait;
 
         [SetUp]
         public void start()
         {
-            driver = new EventFiringWebDriver (new ChromeDriver());
-            driver.FindingElement += (sender, e)=> Console.WriteLine(e.FindMethod);
-            driver.FindElementCompleted += (sender, e) => Console.WriteLine(e.FindMethod + " Found");
-            driver.ExceptionThrown += (sender, e) => Console.WriteLine(e.ThrownException);
-
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
-            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3));
-
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2);
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(2));
         }
+        //{
+        //    private EventFiringWebDriver driver;
+        //    private WebDriverWait wait;
+
+        //    [SetUp]
+        //    public void start()
+        //    {
+        //        driver = new EventFiringWebDriver (new ChromeDriver());
+        //        driver.FindingElement += (sender, e)=> Console.WriteLine(e.FindMethod);
+        //        driver.FindElementCompleted += (sender, e) => Console.WriteLine(e.FindMethod + " Found");
+        //        driver.ExceptionThrown += (sender, e) => Console.WriteLine(e.ThrownException);
+
+        //        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
+        //        wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3));
+
+        //    }
 
         [Test]
         public void FirstTest_Task_17()
         {
-            driver.Url = "http://localhost/litecart/admin/";
+            driver.Url = "http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1";
             driver.FindElement(By.Name("username")).SendKeys("admin");
             driver.FindElement(By.Name("password")).SendKeys("admin");
             driver.FindElement(By.Name("login")).Click();
 
-            Assert.DoesNotThrow(() => driver.FindElement(By.XPath("//th[contains(.,'Statistics')]")));
-            var menus = driver.FindElements(By.Id("app-")).Count;
-            //убрать
-            driver.FindElement(By.CssSelector("_#app-")).Click();
-            Assert.DoesNotThrow(() => driver.FindElement(By.CssSelector("h1")));
-            //убрать
 
+            var setCountProducts = driver.FindElements(By.XPath("//input[starts-with(@name,'products')]/../..//a[not(@title)]")).Count;
+            for (int i = 0; i < setCountProducts; i++)
+            {
+                var selectProducts = driver.FindElements(By.XPath("//input[starts-with(@name,'products')]/../..//a[not(@title)]"));
+                selectProducts[i].Click();
+                Assert.DoesNotThrow(() => driver.FindElement(By.XPath("//h1[contains(.,'Edit Product:')]")));
+                
+                if (driver.Manage().Logs.GetLog("browser").Count > 0)
+                    foreach (LogEntry l in driver.Manage().Logs.GetLog(LogType.Browser))
+                        Console.WriteLine(l);
+                else
+                    Console.WriteLine("логи пустые,страница в списке = {0}", i);
+                Assert.AreEqual(0, driver.Manage().Logs.GetLog("browser").Count);
 
+                driver.Url = "http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1";
+                Assert.DoesNotThrow(() => driver.FindElement(By.XPath("//h1[contains(.,'Catalog')]")));
 
-            //for (var i = 0; i < menus; i++)
-            //{
-            //    var selectMenu = driver.FindElements(By.CssSelector("#app-"));
-            //    selectMenu[i].Click();
+            }
 
-            //    Assert.DoesNotThrow(() => driver.FindElement(By.CssSelector("h1")));
-
-            //    if (driver.FindElements(By.CssSelector("#app-.selected li")).Count > 0)
-            //    {
-            //        var subMenus = driver.FindElements(By.CssSelector("#app-.selected li")).Count;
-            //        for (var j = 0; j < subMenus; j++)
-            //        {
-            //            var selectSubMenu = driver.FindElements(By.CssSelector("#app-.selected li"));
-            //            selectSubMenu[j].Click();
-            //            Assert.DoesNotThrow(() => driver.FindElement(By.CssSelector("h1")));
-            //        }
-            //    }
-            //}
         }
 
         [TearDown]
